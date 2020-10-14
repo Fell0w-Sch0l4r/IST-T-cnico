f45a00810d2f58226b3d8de0afdf6331ac0b29db
Fell0w-Sch0l4r <63580429+Fell0w-Sch0l4r@users.noreply.github.com>
2020-10-14 19:03:54 +0100

    yes

---
+++ teste.py
@@ -0,0 +1,15 @@
+'''print('Vou pedir-lhe dois numeros')
+x = int(input('Escreva o primeiro número, x = '))
+y = int(input('Escreva o segundo número, y = '))
+
+print(f'O valor de ({x} + {y}) X ({x} - {y}) é: {(x + y) * (x - y)}')'''
+
+a = int(input('Distância percorrida em Km: '))
+
+b = int(input('Tempo em minutos:'))
+
+print(f'A velocidade média é:'
+      f'\n{a/(b/60):.2f} km/h'
+      f'\n{ (a*100) / (b*60):.2f} m/s')
+
+
