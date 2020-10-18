ea1a4e43e4385cd311f9e4688bd7b8b50483370e
fell0w-sch0l4r <angelcagiza@gmail.com>
2020-10-18 23:49:02 +0100

    Update 10.py

--- Capítulo 2/10.py
+++ Capítulo 2/10.py
@@ -1,1 +1,15 @@
 num = eval(input('Digite o número: '))
+b=0
+while num != 0:
+    if num % 2 != 0:
+        b = b*10 + num % 10
+        num //= 10
+    else:
+        num //= 10
+
+c = 0
+while b != 0:
+    c = c * 10 + b % 10
+    b //= 10
+
+print(c)
