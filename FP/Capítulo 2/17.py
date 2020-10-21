40d07235fed497c674bd723e866d48679fb27904
Fell0w-Sch0l4r <63580429+Fell0w-Sch0l4r@users.noreply.github.com>
2020-10-21 23:09:06 +0100

    yes

--- FP/Capítulo 2/17.py
+++ FP/Capítulo 2/17.py
@@ -5,9 +5,10 @@ nota = eval(input('Nota (-1 para parar): '))
 while nota != -1:
     if nota > 20 or nota < 0:
         print('Nota inválida')
-    total = total + 1
-    if nota >= 10:
-        positivas = positivas + 1
+    else:
+        total = total + 1
+        if nota >= 10:
+            positivas = positivas + 1
     nota = eval(input('Nota (-1 para parar): '))
 percentagem = (100 * positivas) / total
 print('Houve',positivas,'notas positivas que representa',percentagem,'% dos alunos.')
